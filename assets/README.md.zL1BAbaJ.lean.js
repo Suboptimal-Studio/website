291555adc6be8f16719b6ab95010fc0b4d845687
Stuyk <Stuyk@users.noreply.github.com>
2023-12-08 16:30:25 +0000

    deploy: 38eee3ad871b9d181fb89a32137bd018b82644b0

---
+++ assets/README.md.zL1BAbaJ.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as t,R as s}from"./chunks/framework.UjU5Kp2a.js";const _=JSON.parse('{"title":"Website","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),i={name:"README.md"},n=s("",9),o=[n];function l(p,d,r,c,h,u){return a(),t("div",null,o)}const m=e(i,[["render",l]]);export{_ as __pageData,m as default};
